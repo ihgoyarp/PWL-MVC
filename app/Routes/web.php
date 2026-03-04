7b1b1c059be1b94ef3e5e473437fae4dfcfbf2e5
ihgoyarp <prayoghi.indo@gmail.com>
2026-03-04 10:01:25 +0800

    praktikum kedua, routing

--- app/Routes/web.php
+++ app/Routes/web.php
@@ -2,9 +2,3 @@
 
 Router::get('/', 'HomeController@index');
 Router::get('/about', 'HomeController@about');
-Router::get('/contact', 'HomeController@contact');
-
-
-// Router::get('/mahasiswa', 'MahasiswaController@index');
-// Router::get('/mahasiswa/show/{id}', 'MahasiswaController@show');
-// Router::post('/mahasiswa/store', 'MahasiswaController@store');
